72e604689583d4abee793cf1777c83ce2167cca4
idarlm <101576034+idarlm@users.noreply.github.com>
2024-01-19 19:47:06 +0000

    Implement html wrapper functions

--- hello.hs
+++ hello.hs
@@ -1,1 +1,10 @@
-main = putStrLn "Hello world!"
+main = putStrLn myHtml
+
+myHtml = makeHtml "Hewwo? O.O" "Hewwo, wurld!!! OwO" 
+
+makeHtml title content = html_ $ head_ (title_ title) <> body_ content
+
+html_ content = "<html>" <> content <> "</html>"
+body_ content = "<body>" <> content <> "</body>"
+head_ content = "<head>" <> content <> "</head>"
+title_ t = "<title>" <> t <> "</title>"
