c6c9d1507e77389db48b68d9758784b776bd75fc
idarlm <101576034+idarlm@users.noreply.github.com>
2024-01-20 14:57:59 +0000

    Add types

--- hello.hs
+++ hello.hs
@@ -1,19 +1,43 @@
-main = putStrLn myHtml
+main = putStrLn $ render myHtml
 
-myHtml = makeHtml "Hello" (h1_ "Hello" <> p_ "Welcome to my website")
+myHtml = makeHtml "Hello" (append_ (h1_ "Hello") (p_ "Welcome to my website."))
 
-makeHtml title content = html_ $ head_ (title_ title) <> body_ content
+-- Define newtypes that are backed by strings. 
+-- These can not be used interchangably with other strings
+newtype Html = Html String
+newtype Structure = Structure String
 
-html_ = el "html"
-body_ = el "body"
-head_ = el "head"
-title_ = el "title"
+-- Define type alias for Title. This can be used interchangably with other strings.
+type Title = String
 
-p_ :: String -> String
-p_ = el "p"
+getStructureString :: Structure -> String
+getStructureString (Structure str) = str
 
-h1_ :: String -> String
-h1_ = el "h1"
+append_ :: Structure -> Structure -> Structure
+append_ (Structure a) (Structure b) = Structure (a <> b)
+
+render :: Html -> String
+render html = 
+    case html of
+        Html str -> str
+
+makeHtml :: Title -> Structure -> Html
+makeHtml title (Structure content) = 
+    Html (
+        "<!DOCTYPE html>" <>
+        el "html" (
+            el "head"  (
+                el "title" title
+            ) <> 
+            el "body" content
+        )
+    )
+
+p_ :: String -> Structure
+p_ = Structure . el "p"
+
+h1_ :: String -> Structure
+h1_ = Structure . el "h1"
 
 el :: String -> String -> String
-el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">" 
+el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
