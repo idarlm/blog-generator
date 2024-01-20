a5986cd51c49121e5c42ca6e0a318f3580362d20
idarlm <101576034+idarlm@users.noreply.github.com>
2024-01-20 13:54:28 +0000

    Add more html primitives

--- hello.hs
+++ hello.hs
@@ -1,10 +1,19 @@
 main = putStrLn myHtml
 
-myHtml = makeHtml "Hewwo? O.O" "Hewwo, wurld!!! OwO" 
+myHtml = makeHtml "Hello" (h1_ "Hello" <> p_ "Welcome to my website")
 
 makeHtml title content = html_ $ head_ (title_ title) <> body_ content
 
-html_ content = "<html>" <> content <> "</html>"
-body_ content = "<body>" <> content <> "</body>"
-head_ content = "<head>" <> content <> "</head>"
-title_ t = "<title>" <> t <> "</title>"
+html_ = el "html"
+body_ = el "body"
+head_ = el "head"
+title_ = el "title"
+
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
+
+el :: String -> String -> String
+el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">" 
